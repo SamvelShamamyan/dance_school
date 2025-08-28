d9c9b77b635a0c4a616221e4e855b72144f924c2
Samvel <samvel_shamamyan@ktak.am>
2025-08-28 14:03:54 +0400

    adasdsadasdsds

--- resources/views/admin/school/index.blade.php
+++ resources/views/admin/school/index.blade.php
@@ -5,11 +5,7 @@
     <div class="card-body">
         <div class="row align-items-center">
             <div class="col d-flex align-items-center">
-                <a href="{{ url()->previous() }}" 
-                   class="btn btn-outline-secondary btn-sm mr-3 btn-icon" 
-                   title="Հետ վերադարձ">
-                    <i class="fas fa-arrow-left"></i>
-                </a>
+
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb mb-0 bg-transparent p-0">
                         <li class="breadcrumb-item active d-flex align-items-center" aria-current="page">
